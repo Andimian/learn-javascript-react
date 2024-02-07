4c44d2b872de43ecac640c1287dc3c2622a966af
andimian <andimiangm@gmail.com>
2024-02-07 16:57:22 +0500

    Добавляет селекторы и типизирует их. Создает слайс для ресторанов

--- src/redux/index.tsx
+++ src/redux/index.tsx
@@ -1,5 +1,7 @@
-import { combineSlices, configureStore } from '@reduxjs/toolkit';
+import { combineSlices, configureStore } from "@reduxjs/toolkit";
+import { restaurantSlice }               from "./entities/restaurant";
 
+export type RootState = ReturnType<typeof store.getState>;
 export const store = configureStore({
-	reducer: combineSlices(),
+	reducer: combineSlices(restaurantSlice),
 })

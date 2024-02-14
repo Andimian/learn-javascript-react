46535c65720ec1fff686203a53567375118fe5ad
andimian <andimiangm@gmail.com>
2024-02-14 16:58:41 +0500

    Обновляет слайс для блюд

--- src/redux/entities/dish/index.tsx
+++ src/redux/entities/dish/index.tsx
@@ -5,17 +5,28 @@ import { DishType } from '../../../components/dish/component.tsx';
 // Без типизации не обратиться к полю при получении
 const entityAdapter = createEntityAdapter<DishType>();
 
+// export const dishSlice = createSlice({
+// 	name: 'dish',
+// 	initialState: entityAdapter.getInitialState,
+// 	/* Редьюсеры в слайсах мутируют состояние и ничего не возвращают наружу. Тут подключаем санку. Отличие экстра-редьюсеров
+// 	от обычных в том, что здесь мы не создаем экщен-крейтор, экшены будут создаваться с помощью санки и мы типо говорим,
+// 	 что нам надо обрабатывать экшены, которые не здесь были созданы, а были созданы санкой */
+// 	extraReducers: (builder) =>
+// 		builder.addCase(fetchDish.fulfilled, (state, { payload }) => {
+// 				entityAdapter.setAll(state, payload);
+// 			}
+// 		),
+// });
+
 export const dishSlice = createSlice({
-	name: 'dish',
-	initialState: entityAdapter.getInitialState,
-	/* Редьюсеры в слайсах мутируют состояние и ничего не возвращают наружу. Тут подключаем санку. Отличие экстра-редьюсеров
-	от обычных в том, что здесь мы не создаем экщен-крейтор, экшены будут создаваться с помощью санки и мы типо говорим,
-	 что нам надо обрабатывать экшены, которые не здесь были созданы, а были созданы санкой */
-	extraReducers: (builder) =>
-		builder
-			.addCase(fetchDish.fulfilled, (state, {payload}) => {
-				entityAdapter.setAll(state, payload);
-			}),
-			// .addCase(getDish.rejected, () => {}),
+	name: "dish",
+	initialState: entityAdapter.getInitialState(),
 	reducers: {},
+	extraReducers: (builder) => {
+		// builder.addMatcher()) // можно проверить соответствует ли action по названию (типу) данному matcher-у
+		builder.addCase(getDishesByRestaurantId.fulfilled, (state, { payload }) => {
+			entityAdapter.upsertMany(state, payload);
+		});
+
+	},
 });
--- src/redux/entities/dish/thunks/fetch-dish.tsx
+++ src/redux/entities/dish/thunks/fetch-dish.tsx
@@ -1,11 +1,13 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import { DishType } from '../../../../components/dish/component.tsx';
+import { RootState } from '../../../index.tsx';
 
-export const fetchDish = createAsyncThunk(
+export const fetchDish = createAsyncThunk<DishType[], string, { state: RootState }>(
 	'dish/fetchDish',
 	async () => {
-		const response = await fetch('http://localhost:3001/api/dishes');
+		const response = await fetch('http://localhost:3001/api/dishes', {});
 		const result = await response.json();
 		return result;
 	},
-	// {condition: (_, {getState}) => !selectorDishById(getState()).length}
+	// {condition: (_, {getState}) => !selectorDishIds(getState(), ).length}
 );
